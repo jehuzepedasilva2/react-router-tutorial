881b4fff834e28ce8e13903963d3c95b20152c11
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-04-07 17:06:38 -0700

    need to comeback, not understanding Form

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { 
+  createBrowserRouter, 
+  RouterProvider 
+} from 'react-router-dom';
+import './index.css';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import Root, { 
+  loader as rootLoader, 
+  action as rootAction,
+} from "./routes/root";
+
+const router = createBrowserRouter([
+  // first route is called the root route, since the rest of the 
+  // rest of the routes will render inside it
+  {
+    path: "/", 
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader, 
+    action: rootAction,
+    // if we want this to dynamically change within the root layout, we
+    // add children and use <Outlet /> on root.jsx/parent where we 
+    // want it to show
+    children: [
+      {
+        path: 'contacts/:contactID', 
+        element: <Contact />
+      }
+    ]
+  }, 
+]);
+
+// NOTE: Use <Link> instead of <a> so that switching URL's is done on
+// the client side not web server
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
